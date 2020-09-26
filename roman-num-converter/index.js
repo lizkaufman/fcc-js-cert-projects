9216b06834cb952ede1e45ee0ec8a1e7d36feea8
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-09-26 19:07:42 +0100

    all single digit tests now pass

--- roman-num-converter/index.js
+++ roman-num-converter/index.js
@@ -1,10 +1,39 @@
 //Convert the given number into a roman numeral.
 
+const { formatResultsErrors } = require('jest-message-util');
+
 function convertToRoman(num) {
   let result = [];
-  const romanOne = 'I';
+  const numeralParts = ['I', 'V', 'X'];
   while (num > 0) {
-    result.push(romanOne);
+    switch (true) {
+      case num === 9:
+        result = [...result, numeralParts[0], numeralParts[2]];
+        num = 0;
+        break;
+      case num > 5 && num < 9:
+        result = [
+          ...result,
+          numeralParts[1],
+          new Array(num - 5).fill(numeralParts[0]),
+        ];
+        num = 0;
+        break;
+      case num === 5:
+        result = [...result, numeralParts[1]];
+        num = 0;
+        break;
+      case num === 4:
+        result = [...result, numeralParts[0], numeralParts[1]];
+        num = 0;
+        break;
+      case num < 4:
+        result = [...result, numeralParts[0]];
+        break;
+      default:
+        result = result;
+        console.log('default case');
+    }
     num -= 1;
   }
   return result.join('');
--- roman-num-converter/romanNumConverter.test.js
+++ roman-num-converter/romanNumConverter.test.js
@@ -2,9 +2,8 @@ const convertToRoman = require('./index');
 
 const { TestResult } = require('@jest/types');
 
-// const testCases = [
-//   convertToRoman(2), ✅
-//   convertToRoman(3),
+//   convertToRoman(2),✅
+//   convertToRoman(3),✅
 //   convertToRoman(4),
 //   convertToRoman(5),
 //   convertToRoman(44),
@@ -18,9 +17,8 @@ const { TestResult } = require('@jest/types');
 //   convertToRoman(1023),
 //   convertToRoman(2014),
 //   convertToRoman(3999),
-// ];
 
-describe('Converting two-digit numbers to Roman numerals', () => {
+describe('Converting one-digit numbers to Roman numerals', () => {
   test(`convertToRoman(2) should return 'II'`, () => {
     const expected = 'II';
     const actual = convertToRoman(2);
@@ -31,6 +29,27 @@ describe('Converting two-digit numbers to Roman numerals', () => {
     const actual = convertToRoman(3);
     expect(actual).toBe(expected);
   });
+  test(`convertToRoman(4) should return 'IV'`, () => {
+    const expected = 'IV';
+    const actual = convertToRoman(4);
+    expect(actual).toBe(expected);
+  });
+  test(`convertToRoman(5) should return 'V'`, () => {
+    const expected = 'V';
+    const actual = convertToRoman(5);
+    expect(actual).toBe(expected);
+  });
+  test(`convertToRoman(6) should return 'VI'`, () => {
+    const expected = 'VI';
+    const actual = convertToRoman(6);
+    expect(actual).toBe(expected);
+  });
+  test(`convertToRoman(9) should return 'IX'`, () => {
+    const expected = 'IX';
+    const actual = convertToRoman(9);
+    expect(actual).toBe(expected);
+  });
 });
+// describe('Converting two-digit numbers to Roman numerals', () => {});
 // describe('Converting three-digit numbers to Roman numerals', () => {});
 // describe('Converting four-digit numbers to Roman numerals', () => {});
