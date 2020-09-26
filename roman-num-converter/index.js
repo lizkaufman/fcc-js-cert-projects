aede9a957a343eb8064fa67a1e4ccff74ad99642
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-09-26 19:45:16 +0100

    first three two-dig tests passing

--- roman-num-converter/index.js
+++ roman-num-converter/index.js
@@ -7,18 +7,50 @@ function convertToRoman(num) {
   //1, 5, 10, 50, 100, 500, 1000
   let result = [];
   //---Breaking the number down:---
+  //TODO: Can move this below to its own function to abstract away!
   let remaining = num;
-  const thousands = Math.floor(remaining / 1000);
+  let thousands = Math.floor(remaining / 1000);
   remaining = remaining % 1000;
-  const hundreds = Math.floor(remaining / 100);
+  let hundreds = Math.floor(remaining / 100);
   remaining = remaining % 100;
-  const tens = Math.floor(remaining / 10);
-  ones = remaining % 10;
+  let tens = Math.floor(remaining / 10);
+  let ones = remaining % 10;
   console.log({ thousands, hundreds, tens, ones });
-  //thousands:
-  //hundreds:
-  //tens:
-  //ones:
+  //---thousands---:
+  //---hundreds---:
+  //---tens---:
+  while (tens > 0) {
+    switch (true) {
+      case tens === 9:
+        result = [...result, numeralParts[3], numeralParts[4]];
+        tens = 0;
+        break;
+      case tens > 5 && tens < 9:
+        result = [
+          ...result,
+          numeralParts[3],
+          ...new Array(tens - 5).fill(numeralParts[2]),
+        ];
+        tens = 0;
+        break;
+      case tens === 5:
+        result = [...result, numeralParts[3]];
+        tens = 0;
+        break;
+      case tens === 4:
+        result = [...result, numeralParts[2], numeralParts[3]];
+        tens = 0;
+        break;
+      case tens < 4:
+        result = [...result, numeralParts[2]];
+        break;
+      default:
+        result = result;
+        console.log('default case');
+    }
+    tens -= 1;
+  }
+  //---ones---:
   while (ones > 0) {
     switch (true) {
       case ones === 9:
--- roman-num-converter/romanNumConverter.test.js
+++ roman-num-converter/romanNumConverter.test.js
@@ -4,43 +4,68 @@ describe('Converting one-digit numbers to Roman numerals', () => {
   test(`convertToRoman(2) should return 'II'`, () => {
     const expected = 'II';
     const actual = convertToRoman(2);
-    expect(actual).toBe(expected);
+    expect(actual).toBe(expected); //✅
   });
   test(`convertToRoman(3) should return 'III'`, () => {
     const expected = 'III';
     const actual = convertToRoman(3);
-    expect(actual).toBe(expected);
+    expect(actual).toBe(expected); //✅
   });
   test(`convertToRoman(4) should return 'IV'`, () => {
     const expected = 'IV';
     const actual = convertToRoman(4);
-    expect(actual).toBe(expected);
+    expect(actual).toBe(expected); //✅
   });
   test(`convertToRoman(5) should return 'V'`, () => {
     const expected = 'V';
     const actual = convertToRoman(5);
-    expect(actual).toBe(expected);
+    expect(actual).toBe(expected); //✅
   });
   test(`convertToRoman(6) should return 'VI'`, () => {
     const expected = 'VI';
     const actual = convertToRoman(6);
-    expect(actual).toBe(expected);
+    expect(actual).toBe(expected); //✅
   });
   test(`convertToRoman(9) should return 'IX'`, () => {
     const expected = 'IX';
     const actual = convertToRoman(9);
-    expect(actual).toBe(expected);
+    expect(actual).toBe(expected); //✅
   });
 });
 describe('Converting two-digit numbers to Roman numerals', () => {
   test(`convertToRoman(10) should return 'X'`, () => {
     const expected = 'X';
     const actual = convertToRoman(10);
-    expect(actual).toBe(expected);
+    expect(actual).toBe(expected); //✅
   });
   test(`convertToRoman(23) should return 'XXIII'`, () => {
     const expected = 'XXIII';
     const actual = convertToRoman(23);
+    expect(actual).toBe(expected); //✅
+  });
+  test(`convertToRoman(45) should return 'XLV'`, () => {
+    const expected = 'XLV';
+    const actual = convertToRoman(45);
+    expect(actual).toBe(expected); //✅
+  });
+  test.only(`convertToRoman(68) should return 'LXVIII'`, () => {
+    const expected = 'LXVIII';
+    const actual = convertToRoman(68);
+    expect(actual).toBe(expected);
+  });
+  test(`convertToRoman(83) should return 'LXXXIII'`, () => {
+    const expected = 'LXXXIII';
+    const actual = convertToRoman(83);
+    expect(actual).toBe(expected);
+  });
+  test(`convertToRoman(97) should return 'XCVII'`, () => {
+    const expected = 'XCVII';
+    const actual = convertToRoman(97);
+    expect(actual).toBe(expected);
+  });
+  test(`convertToRoman(99) should return 'XCIX'`, () => {
+    const expected = 'XCIX';
+    const actual = convertToRoman(99);
     expect(actual).toBe(expected);
   });
 });
